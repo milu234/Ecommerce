2dc2124b62545a5603dd420e02f2cee31e6cff0f
milu234 <milanhazra234.mh@gmail.com>
2019-06-26 18:23:48 +0530

    login done

--- shop/app/Http/Controllers/AdminController.php
+++ shop/app/Http/Controllers/AdminController.php
@@ -1,10 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+use Auth;
+
 
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    //login function
+    public function login(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->input();
+            if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password'],'admin'=>'1'])){
+                echo "Success";die;
+            }else{
+                echo "Failed";die;
+            }
+        }
+
+        return view('admin.admin_login');
+    }
 }
--- shop/routes/web.php
+++ shop/routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/admin','AdminController@login');
+
+Route::match(['get', 'post'], '/admin' , 'AdminController@login');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
